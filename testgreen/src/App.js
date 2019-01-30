56300e0d6f44aaecc429ed8bf05406267b85a3c2
vanceleon <vanceleon44@gmail.com>
2019-01-29 20:07:40 -0500

    configuring the testing page

--- testgreen/src/App.js
+++ testgreen/src/App.js
@@ -3,9 +3,9 @@ import Landing from './LandingPage';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className='App' style={{margin: '0 auto', maxWidth: '1000px'}}>
         Testing the GREENSOCK
-        <img src="https://sallysbakingaddiction.com/wp-content/uploads/2018/07/best-black-bean-burgers-2.jpg" alt="burger"/>
+        <img src="https://sallysbakingaddiction.com/wp-content/uploads/2018/07/best-black-bean-burgers-2.jpg" alt="burger" style={{height: '300px', }}/>
         <Landing />
       </div>
     );
